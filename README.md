c87e229227ddaed6fff5cd28fbd89ece0e21ce96
Adam Brown <adam@novoda.com>
2014-04-13 11:04:54 +0100

    adding coveralls to readme and made the dumper file conistent

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Pokemon red save parser and displayer
 
 
 [![Build Status](https://travis-ci.org/ouchadam/PKMN-Scoreboard.svg?branch=master)](https://travis-ci.org/ouchadam/PKMN-Scoreboard)
+
+[![Coverage Status](https://coveralls.io/repos/ouchadam/PKMN-Scoreboard/badge.png)](https://coveralls.io/r/ouchadam/PKMN-Scoreboard)
--- src/main/java/com/ouchadam/psr/Dumper.java
+++ src/main/java/com/ouchadam/psr/Dumper.java
@@ -1,16 +1,15 @@
 package com.ouchadam.psr;
 
+import com.ouchadam.psr.read.PokemonFile;
 import com.ouchadam.psr.read.SpeciesToPokedex;
 import com.ouchadam.psr.read.domain.Money;
 import com.ouchadam.psr.read.domain.Playtime;
 import com.ouchadam.psr.read.domain.Pokemon;
 import com.ouchadam.psr.read.domain.Team;
-import com.ouchadam.psr.read.file.PokemonState;
-import com.ouchadam.psr.read.file.RandomFileAccessWrapper;
 import com.ouchadam.psr.read.reader.MoneyReader;
-import com.ouchadam.psr.read.reader.TeamReader;
 import com.ouchadam.psr.read.reader.PlaytimeReader;
 import com.ouchadam.psr.read.reader.TeamOffsets;
+import com.ouchadam.psr.read.reader.TeamReader;
 import com.ouchadam.psr.read.text.TextReader;
 import com.ouchadam.psr.read.text.TextTerminatedException;
 import com.ouchadam.psr.read.text.TextUnhandledException;
@@ -19,14 +18,14 @@ import java.io.IOException;
 
 public class Dumper {
 
-    public static void dumpAll(long start, long end, RandomFileAccessWrapper file) throws IOException {
+    public static void dumpAll(long start, long end, PokemonFile file) throws IOException {
         for (long index = start; index < end; index++) {
             int aByte = file.getInt(index);
             System.out.print(aByte + " ");
         }
     }
 
-    public static void printAllText(long start, long end, RandomFileAccessWrapper file) {
+    public static void printAllText(long start, long end, PokemonFile file) {
         String text = "";
         TextReader textReader = new TextReader();
         for (long index = start; index < end; index++) {
@@ -49,12 +48,12 @@ public class Dumper {
 
     }
 
-    public static void dumpMoney(PokemonState state, Money wanted) {
+    public static void dumpMoney(PokemonFile file, Money wanted) {
         System.out.println("Start");
-        for (int i = 0; i < state.length() - 3; i++) {
+        for (int i = 0; i < file.length() - 3; i++) {
             MoneyReader moneyReader = new MoneyReader(i);
             try {
-                Money money = moneyReader.read(state);
+                Money money = moneyReader.read(file);
                 if (money.equals(wanted)) {
                     System.out.println("Boom! : " + Integer.toHexString(i) + " : " + money.formatted());
                     System.out.println("");
@@ -65,12 +64,12 @@ public class Dumper {
         System.out.println("End");
     }
 
-    public static void dumpPlaytime(PokemonState state, Playtime wanted) {
+    public static void dumpPlaytime(PokemonFile file, Playtime wanted) {
         System.out.println("Start");
-        for (int i = 0; i < state.length() - 3; i++) {
+        for (int i = 0; i < file.length() - 3; i++) {
             PlaytimeReader moneyStateReader = new PlaytimeReader(i);
             try {
-                Playtime playtime = moneyStateReader.read(state);
+                Playtime playtime = moneyStateReader.read(file);
                 if (playtime.equals(wanted)) {
                     System.out.println("Boom! : " + Integer.toHexString(i) + " : " + playtime.formatted());
                     System.out.println("");
@@ -81,14 +80,14 @@ public class Dumper {
         System.out.println("End");
     }
 
-    public static void dumpPokemon(PokemonState state, String name, int level) {
+    public static void dumpPokemon(PokemonFile file, String name, int level) {
         System.out.println("Start");
         TextReader textReader = new TextReader();
         SpeciesToPokedex speciesToPokedex = new SpeciesToPokedex();
-        for (int i = 0; i < state.length() - 3; i++) {
+        for (int i = 0; i < file.length() - 3; i++) {
             TeamReader moneyStateReader = new TeamReader(textReader, speciesToPokedex, new TeamOffsets(i));
             try {
-                Team team = moneyStateReader.read(state);
+                Team team = moneyStateReader.read(file);
                 Pokemon pokemon = team.pokemons.get(0);
                 if (pokemon.getName().equals(name) && pokemon.getLevel() == level) {
                     System.out.println("Boom! : " + Integer.toHexString(i));
